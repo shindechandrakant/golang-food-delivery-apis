94afd39f28fea7483ccf68d5b92a9128d593e045
Chandrakant <chandrakantshinde7348@gmail.com>
2026-03-15 20:00:20 +0530

    add: success & response structure

---
+++ internal/utils/response.go
@@ -0,0 +1,17 @@
+package utils
+
+import "github.com/gofiber/fiber/v3"
+
+func SuccessResponse(ctx fiber.Ctx, data interface{}) error {
+	return ctx.JSON(fiber.Map{
+		"success": true,
+		"data":    data,
+	})
+}
+
+func ErrorResponse(ctx fiber.Ctx, message string) error {
+	return ctx.Status(400).JSON(fiber.Map{
+		"success": false,
+		"error":   message,
+	})
+}
